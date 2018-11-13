afad82bb612fd2ed57c245b75be698c7f97e1a07
sk858 <sk858@cornell.edu>
2018-11-13 18:14:25 -0500

    Added setup unit page

--- src/components/NumericMeasurement.vue
+++ src/components/NumericMeasurement.vue
@@ -2,13 +2,16 @@
   <div>
     <Layout v-bind:title="this.$route.params.title" type="1" v-bind:settingsList="['Add Record','View List','View Chart']">
     </Layout>
-      <b-container class="valueCell indentationOneOf">
-        
+      <b-container class="valueContainer indentationOneOf">
+        <b-row align-v="center" class="valueCell">
+            <b-form-input id="valueForm" size="lg" v-model="text" type="text" placeholder="Enter your unit"></b-form-input>
+        </b-row>
       </b-container>
-      <b-container class="timerButton">
-        <div class="indentationTwoOff"></div>
-        
-      </b-container>
+        <b-container class="saveContainer indentationOneOf">
+            <b-row align-v="center">
+                <b-button id="saveButton">SAVE</b-button>
+            </b-row>
+        </b-container>
   </div>
 </template>
 
@@ -47,9 +50,39 @@ export default {
   margin-top: 25%;
   height: 165px;
   color: black;
-  width: 90%;
+  width: 100%;
   border-radius: 5px;
+  margin-left: unset;
+  margin-right: unset;
 }
+#valueForm{
+    background-color:white;
+    border-bottom: none;
+    text-align: center;
+    margin-left: auto;
+    margin-right:auto;
+    height: 165px;
+    font-size: 44px;
+    font-weight: bold;
+    color:black;
+}
+
+.saveContainer{
+    padding-top: 30px;
+   
+}
+
+#saveButton{
+    width: 100%;
+    background-color: #01a76b;
+    height:70px;
+    font-size: 44px;
+    text-align: center;
+}
+.valueContainer{
+    background-color:white;
+}
+
 .timerDescription{
   padding-top: 60px;
   font-size: 4em;
